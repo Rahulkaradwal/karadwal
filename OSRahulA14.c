f5fa184e6bf31cbce649fc044203ef8482fd7da2
Rahulkaradwal <49285978+Rahulkaradwal@users.noreply.github.com>
2019-04-09 19:25:14 +0530

    Update OSRahulA14.c

--- OSRahulA14.c
+++ OSRahulA14.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int bt[20],k,p[20],pr[20],wt[20],tat[20],i,j,n,total=0,pos,temp,base=60,st[10],b1,big=4,l;
-    int totwt=0,totta=0,ft[19];
-    float awt,ata,a[10];
-
+    int bt[20],k,p[20],pr[20],wt[20],tat[20],i,j,n,total=0,pos,temp,base=60,start[10],b1=0,big=4,l,num[100],ar[100];
+    int totwait=0,totturn=0,final[19],a[10];
+    float avgwt,avgta;
+	printf("\n");
    	printf("**********************************************TRADITIONAL UNIX SCHEDULER*******************************************\n\n\n");
 
     printf("Enter Total Number of Process:");
@@ -16,14 +16,20 @@ int main()
         printf("\nP[%d]\n",i+1);
         printf("Burst Time:");
         scanf("%d",&bt[i]);
-        b1=b1+bt[i];
+        num[i]=bt[i];
+        b1=b1+bt[i];		//total time
         printf("Arrival Time:");
         scanf("%d",&a[i]);
-   	printf("Initially Priority ");
-		scanf("%d",&pr[i]);           
+        ar[i]=a[i];
+        printf("Initially Priority ");
+		scanf("%d",&pr[i]);     
+		printf("\n");     
+		p[i]=i+1; 
     }
+
+	
     
-    for(k=0;k<n;k++)
+    for(k=0;k<n;k++)  //cPu ussage
     {
     	a[k]=0;
     }
@@ -40,15 +46,61 @@ int main()
 		}
 		a[l]=a[l]+1;
 		bt[l]=bt[l]-1;
+		if(bt[l]=0)
+		{
+			bt[l]==0;
+		}
 		for(k=0;k<n;k++)
 		{
-			pr[k]=(a[k]/2)*60;
+			pr[k]=(a[k]/2)*base;
 		}
         
 	}
+	printf("Recalculated Priority of Process\n");
 	for(i=0;i<n;i++)
 	{
-		printf("Recalculated Priority of Process %d: %d\n",i+1,pr[i]);
+		printf("P[%d]: %d\n",i+1,pr[i]);
 	}
-
-}
+    
+    
+    
+      //sorting burst time, priority and process number in ascending order using selection sort
+    
+	for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=0;j<n;j++)
+        {
+            if(pr[j]<pr[pos])
+            {
+			
+                pos=j;
+ 
+        temp=pr[i];
+        pr[i]=pr[pos];
+        pr[pos]=temp;
+ 
+        temp=bt[i];
+        bt[i]=bt[pos];
+        bt[pos]=temp;
+ 
+        temp=p[i];
+        p[i]=p[pos];
+        p[pos]=temp;
+        
+         temp=ar[i];
+        ar[i]=ar[pos];
+        ar[pos]=temp;
+        pos++;
+    }
+    }
+    }  	
+  
+  
+  	printf("\t\t\t\t VALUES AFTER RECALCULATEING THE NEW PRIORITIES");
+  	 printf("\n");
+  	 fflush(stdin);
+    printf("\n\n\nProcess Number\t\t\t\tBurst Time\t\t\tArrival Time\t\t\tPriority\n");
+	for(i=0;i<n;i++){
+	printf("    P[%d]\t\t\t\t    %d\t\t\t\t     %d\t\t\t\t    %d\n",i+1,num[i],ar[i],pr[i]);
+	}
